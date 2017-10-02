e867457f50f351d5ee48ed938c99bd2571975e1d
gulliverbear <gulliver.bear@gmail.com>
2017-10-01 20:43:46 -0500

    Add files via upload

---
+++ word_find_solver.py
@@ -0,0 +1,69 @@
+
+#example word find puzzle
+l = [
+'nllabaqgirlxhezcfabqlvtdgzten',
+'imbvtlopemituymtxxgametmwibeh',
+'goonenetjwxkyszaztozlfzgkbrpv',
+'awyuhwqpbugeviflgqesclnineele',
+'mssnevesntwoucvgojwniygrztxnm',
+'enufpxicqqhomeballufdxbltyfjo',
+'puknxxjmjtkflkorvctcbrifhovjh',
+'hlrvxfvtdshbeoywgkwwfeofrbxkj',
+'oymvilwghjbgnteotkvqodrwepguw',
+'mzhvvoteirfeicbekfabpflseipho',
+'exejminsrgeanenueqbloojcrzthr',
+'aeihvioirmsewgcuvhxudyylluwjd',
+'lkognxxtitccynkniorifkufudowy',
+'tmorkhothfztflezfddusorisnhce',
+'ejnfqsyrwbqeblvvifxpwnpvzehwm',
+'ametereydsenoxfdeighthcetvkqa',
+'knweueqvprfiymoqmssyitnqmestg',
+'duuovcesezoyhtuggeerhtpujsmwz',
+'gmftktnmlngwtbrhbbqjabftfreog',
+'eiqycwhnifooonlqoowtfdgzamrzh',
+'ncruofmgqtlfwyemymsixalnytujz',
+'uzwlbnndivuktxpvtwsmtlbekhozp',
+'fwgvjlbotevecvthefxeasxtngfnt',
+'sojxdbnqxxvetdelkstbveontiyoi',
+'grrilebbvgvrmfivezrxigenbenlm',
+'edbspveninvhfuleighteefweeile',
+'mpaonrcmdohtfyuepkndmumzoltad',
+'atlgirloweumdcwordupilbozrgbz',
+'grlzijfunxyemagstgfptyvchydkh'
+]
+
+words = [
+'one',
+'two',
+'three',
+'four',
+'five',
+'six',
+'seven',
+'eight',
+'nine',
+'ten',
+'boy',
+'money',
+'girl',
+'star',
+'game',
+'easy',
+'dollar',
+'home',
+'time',
+'ball',
+'word',
+'color',
+'life',
+'happy',
+'fun'
+]
+
+n_row = len(l)
+n_col = len(l[0])
+found = 0
+
+# to do: implement the search to find all words in the grid
+
+
